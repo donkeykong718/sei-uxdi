6b94ea086873b49e752896033f7d08245d1809c7
richardsaudek <richard.saudek@gmail.com>
2023-03-14 11:19:20 -0400

    logo,hamburger and icons in position

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import NavBar from './components/NavBar.jsx';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,51 @@
-export default function NavBar() {
+import React from "react";
+import Moo from "../assets/Icons/Moo-51.svg";
+import HamburgerIcon from "../assets/Icons/HamburgerLight.svg";
+import UserIconLight from "../assets/Icons/UserVector.svg";
+import ShoppingCart from "../assets/Icons/CartVector.svg";
+import LightToggleIcon from "../assets/Icons/LightToggle.svg";
 
-  <div class="Rectangle-17"></div>
+function NavBar() {
+  return (
+    <div className="NavBarRectangle-17">
+      <div className="NavBarRectangle-16">
+            <div className="Moo-ChaCha Logo-Text-Style">
+              <span className="logo-text">Moo</span>
+              <img src={Moo} alt="" />
+              <span className="logo-text">ChaCha</span>
+            </div>
+        <div className="LightDarkButtonBackground">
+          <div className="LightDarkButtonKnob">
+            {/* <div className="LightToggleIcon"></div> */}
+            <img src={LightToggleIcon} alt="" />
+            <div className="DarkToggleIcon"></div>
 
-  return {
-  }
+          </div>
+        </div>
+      </div>
+
+      <div className="NavBarRectangle-15">
+        <div className="HamburgerLight">
+          <img
+            src={HamburgerIcon}
+            alt="Hamburger Icon"
+            width="30"
+            height="22"
+          />
+        </div>
+        <div className="UserIconLight">
+          <img src={UserIconLight} alt="User Icon" className="icon" />
+        </div>
+        <div className="ShoppingCartLight">
+          <img
+            src={ShoppingCart}
+            alt="Shoping Cart Icon"
+            className="icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default NavBar;
